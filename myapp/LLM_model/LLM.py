9658e96556b9c330e0842c6254892b3321fbd2f1
vinay <vinaykumar.deshmukh@zapuza.in>
2025-02-17 10:18:19 +0530

    Changed token.

--- myapp/LLM_model/LLM.py
+++ myapp/LLM_model/LLM.py
@@ -4,7 +4,7 @@ import replicate
 # Initialize the Replicate client
 def suggestions(age, gender, ap_hi, ap_lo, active, smoke, cholesterol, glucose, model_prediction):
     client = replicate.Client(
-        api_token="r8_8vMPpCw9hEY5nZH3F5EFWHI3UH3pTzO3LtfV5"  # Replace with your actual API key
+        api_token="r8_0HzSA00FtuarpIgzJ76sPEOdDwNeJuC09ajDO"  # Replace with your actual API key
     )
 
     gender = 'male' if gender == 'M' else 'female'
